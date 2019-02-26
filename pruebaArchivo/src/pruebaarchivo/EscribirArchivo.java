f0ec8a880e4f9b3511c63e13d55098940b3df97a
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-02-26 11:11:06 +0100

    Hace falta refactor/limpiar bastante xd

--- pruebaArchivo/src/pruebaarchivo/EscribirArchivo.java
+++ pruebaArchivo/src/pruebaarchivo/EscribirArchivo.java
@@ -7,11 +7,13 @@ package pruebaarchivo;
  */
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EscribirArchivo {
     File archivo = null;
     PrintWriter f = null;
+    Scanner leer = new Scanner(System.in);
     
     public File escribirNumeros(String ruta){
         archivo = new File(ruta+".txt");
@@ -23,5 +25,34 @@ public class EscribirArchivo {
             System.out.println(e.getMessage());
         }
         return archivo;
+    }
+    
+    public void escribirLineas(String nomeFich,ArrayList<Alumno>lista){
+        try{
+            f = new PrintWriter(new File(nomeFich+".txt"));
+            for(Alumno a1:lista){
+                f.println(a1.getNome()+","+a1.getNota());
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }finally{
+            f.close();
+        }
+    }
+    
+    public void engadir(String nomeFich){
+        try{
+            f = new PrintWriter(new FileWriter(nomeFich+".txt",true));
+            Alumno a = new Alumno();
+            System.out.println("Dame nombre");
+            a.setNome(leer.next());
+            System.out.println("Dame nota");
+            a.setNota(leer.nextInt());
+            f.println(a);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }finally{
+            f.close();
+        }
     }
 }
--- pruebaArchivo/src/pruebaarchivo/PruebaArchivo.java
+++ pruebaArchivo/src/pruebaarchivo/PruebaArchivo.java
@@ -9,12 +9,14 @@ package pruebaarchivo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PruebaArchivo {
 
     public static void main(String[] args) throws FileNotFoundException {
         File f;
         Leer obx = new Leer();
+        Scanner leer = new Scanner(System.in);
         
         f = new File("/home/local/DANIELCASTELAO/gpietrafesavieitez/archivo.txt");
         obx.porLineas(f);
@@ -33,5 +35,15 @@ public class PruebaArchivo {
         ArrayList<Alumno> listaAlumnos = new ArrayList<>();
         EscribirArchivo obxEscribir = new EscribirArchivo();
         obxEscribir.escribirNumeros("num");
+        
+        for(int i = 0; i < 4; i++){
+            Alumno a = new Alumno();
+            System.out.println("Dame nombre");
+            a.setNome(leer.next());
+            System.out.println("Dame nota");
+            a.setNota(leer.nextInt());
+            listaAlumnos.add(a);
+        }
+        obxEscribir.escribirLineas("alumnado", listaAlumnos);
     }
 }
