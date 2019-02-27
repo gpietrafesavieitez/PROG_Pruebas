35903c68d0a07df50a7fd5a178e5e32780a59f19
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-02-27 12:20:13 +0100

    Refactor + fixed

--- ejemploArchivoSerializable/src/ejemploarchivoserializable/EscrituraSerializable.java
+++ ejemploArchivoSerializable/src/ejemploarchivoserializable/EscrituraSerializable.java
@@ -8,9 +8,10 @@ package ejemploarchivoserializable;
 
 import java.io.*;
 import java.util.Scanner;
+import utilidades.PedirDatos;
 
 public class EscrituraSerializable {
-    ObjectOutputStream fich;
+    ObjectOutputStream o;
     FileOutputStream f;
     Scanner leer = new Scanner(System.in);
     
@@ -19,26 +20,26 @@ public class EscrituraSerializable {
             // Crea el fichero de datos
             f = new FileOutputStream(nome + ".dat");
             // Abro el fichero en modo escritura y lo conecto con f
-            fich = new ObjectOutputStream(f);
+            o = new ObjectOutputStream(f);
             // fich = new ObjectOutputStream(new FileOutputStream(nome+".dat"));
             for(int i = 0; i < 3; i ++){
-                System.out.println("Dame nombre:");
-                String nombre = leer.next();
-                System.out.println("Dame nota:");
-                int nota = leer.nextInt();
-                Alumno al = new Alumno(nombre,nota);
-                fich.writeObject(al);
+                Alumno al = new Alumno(PedirDatos.tipoString("Nombre:"),PedirDatos.tipoInt("Dame nota:"));
+                o.writeObject(al);
             }
-        }catch(FileNotFoundException e){
+        }catch(FileNotFoundException e1){
             System.out.println("Error: Clase no encontrada.");
-        }catch(IOException e1){ // Excepcion superior 
+        }catch(IOException e2){ // Excepcion superior 
             System.out.println("Error: Problema de entrada/salida.");
+        }catch(NumberFormatException e3){
+            System.out.println("Error: Formato inválido.");
+        }catch(Exception e){
+            System.out.println("Error: Excepción no controlada.");
         }finally{
             try{ // Por si hay problemas al cerrar
-                fich.close();
+                o.close();
                 f.close();
             }catch (IOException e3){
-                System.out.println("Error: Problema de entrada/salida.");
+                System.out.println("Error: Problema de entrada/salida del fichero.");
             }
         }
     }
--- ejemploArchivoSerializable/src/ejemploarchivoserializable/LecturaSerializable.java
+++ ejemploArchivoSerializable/src/ejemploarchivoserializable/LecturaSerializable.java
@@ -7,31 +7,31 @@
 package ejemploarchivoserializable;
 
 import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class LecturaSerializable {
-    ObjectInputStream ler;
+    ObjectInputStream o;
     FileInputStream f;
     Alumno al;
     
     public void leerObjSerial(String nombreArchivo){
         try{
             f = new FileInputStream(nombreArchivo + ".dat");
-            ler = new ObjectInputStream(f);
+            o = new ObjectInputStream(f);
             do{
-                al = (Alumno) ler.readObject();
-                System.out.println(al);
+                try{
+                    al = (Alumno) o.readObject();
+                    System.out.println(al);
+                }catch(Exception e){
+                    break;
+                }
             }while(al != null);
-        }catch(ClassNotFoundException e){
-            System.out.println("Error: Clase no encontrada.");
         }catch(FileNotFoundException e1){
             System.out.println("Error: Archivo no encontrado.");
         }catch(IOException e2){
             System.out.println("Error: Problema de entrada/salida.");
         }finally{
             try{
-                ler.close();
+                o.close();
                 f.close();
             }catch (IOException e3){
                 System.out.println("Error: Problema de entrada/salida.");
